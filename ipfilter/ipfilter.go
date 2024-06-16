77bf4b3fdb7cab7b8917fd06ec040081ec777f59
lumavpn <admin@lumavpn.com>
2024-06-16 13:27:50 -0700

    Add comments

--- ipfilter/ipfilter.go
+++ ipfilter/ipfilter.go
@@ -36,10 +36,11 @@ func IsRemoteAddrDisAllowed(addr net.Addr) bool {
 	return isAllowed(m.AddrPort().Addr().Unmap()) && !isDisAllowed(m.AddrPort().Addr().Unmap())
 }
 
-func isAllowed(addr netip.Addr) bool {
-	if addr.IsValid() {
-		for _, prefix := range lanAllowedIPs {
-			if prefix.Contains(addr) {
+// contains returns whether or not any of the given networks includes ip.
+func contains(networks []netip.Prefix, ip netip.Addr) bool {
+	if ip.IsValid() {
+		for _, prefix := range networks {
+			if prefix.Contains(ip) {
 				return true
 			}
 		}
@@ -47,13 +48,12 @@ func isAllowed(addr netip.Addr) bool {
 	return false
 }
 
-func isDisAllowed(addr netip.Addr) bool {
-	if addr.IsValid() {
-		for _, prefix := range lanDisAllowedIPs {
-			if prefix.Contains(addr) {
-				return true
-			}
-		}
-	}
-	return false
+// isAllowed returns whether or not the list of allowed IPs includes ip.
+func isAllowed(ip netip.Addr) bool {
+	return contains(lanAllowedIPs, ip)
+}
+
+// isDisAllowed returns whether or not the list of disallowed IPs includes ip.
+func isDisAllowed(ip netip.Addr) bool {
+	return contains(lanDisAllowedIPs, ip)
 }
