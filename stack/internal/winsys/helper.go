64e99c8fd7f5059e953b20aa10c658036db76952
lumavpn <admin@lumavpn.com>
2024-06-18 16:52:27 -0700

    Add wintun

---
+++ stack/internal/winsys/helper.go
@@ -0,0 +1,45 @@
+//go:build windows
+
+package winsys
+
+import (
+	"os"
+	"unsafe"
+
+	"github.com/lumavpn/luma/util"
+	"golang.org/x/sys/windows"
+)
+
+func CreateDisplayData(name, description string) FWPM_DISPLAY_DATA0 {
+	namePtr, err := windows.UTF16PtrFromString(name)
+	util.Must(err)
+
+	descriptionPtr, err := windows.UTF16PtrFromString(description)
+	util.Must(err)
+
+	return FWPM_DISPLAY_DATA0{
+		Name:        namePtr,
+		Description: descriptionPtr,
+	}
+}
+
+func GetCurrentProcessAppID() (*FWP_BYTE_BLOB, error) {
+	currentFile, err := os.Executable()
+	if err != nil {
+		return nil, err
+	}
+
+	curFilePtr, err := windows.UTF16PtrFromString(currentFile)
+	if err != nil {
+		return nil, err
+	}
+
+	windows.GetCurrentProcessId()
+
+	var appID *FWP_BYTE_BLOB
+	err = FwpmGetAppIdFromFileName0(curFilePtr, unsafe.Pointer(&appID))
+	if err != nil {
+		return nil, err
+	}
+	return appID, nil
+}
