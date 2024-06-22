f5eb017cbb61261955d5e12e1d04ab8ef248126f
lumavpn <admin@lumavpn.com>
2024-06-22 08:58:49 -0700

    Add rule package and proto Makefile target

---
+++ util/file.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"errors"
+	"os"
+)
+
+func FileExists(path string) (bool, error) {
+	fi, err := os.Stat(path)
+	if err == nil {
+		if fi.Mode()&os.ModeType == 0 {
+			return true, nil
+		}
+		return false, errors.New(path + " exists but is not regular file")
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
